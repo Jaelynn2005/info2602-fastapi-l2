830a239a7ee226b257b785c7a0674a05fd29304c
Jaelynn <jaelynnredhead19@gmail.com>
2026-02-13 10:21:01 -0400

    Lab 2 completed with exercises

--- app/cli.py
+++ app/cli.py
@@ -3,46 +3,106 @@ from app.database import create_db_and_tables, get_session, drop_all
 from app.models import User
 from fastapi import Depends
 from sqlmodel import select
+from sqlalchemy import or_
 from sqlalchemy.exc import IntegrityError
 
 cli = typer.Typer()
 
-@cli.command()
+@cli.command(help="Initialize the database with some data")
 def initialize():
     with get_session() as db: # Get a connection to the database
         drop_all() # delete all tables
         create_db_and_tables() #recreate all tables
-        bob = User('bob', 'bob@mail.com', 'bobpass') # Create a new user (in memory)
+        bob = User(username='bob', email='bob@mail.com', password='bobpass') # Create a new user (in memory)
         db.add(bob) # Tell the database about this new data
         db.commit() # Tell the database persist the data
         db.refresh(bob) # Update the user (we use this to get the ID from the db)
         print("Database Initialized")
 
-@cli.command()
-def get_user(username:str):
-    # The code for task 5.1 goes here. Once implemented, remove the line below that says "pass"
-    pass
+#retrieving a user by their username and printing it
+@cli.command(help="Get a user by their username")
+def get_user(username:str): #takes the username argument
+    with get_session() as db:
+        user = db.exec(select(User).where(User.username==username)).all()
+        if not user:
+            print(f'{username} not found')
+            return
+        print(user)
 
-@cli.command()
+#retrieving all users in the db
+@cli.command(help="Get all users in the database")
 def get_all_users():
-    # The code for task 5.2 goes here. Once implemented, remove the line below that says "pass"
-    pass
+    with get_session() as db:
+        all_users = db.exec(select(User)).all()
+        if not all_users:
+            print("Users not found")
+        else:
+            for user in all_users: #using a for loop to print each user one by one.
+                print(user)
 
-
-@cli.command()
+#updating user bob's email
+@cli.command(help="Change a user's email")
 def change_email(username: str, new_email:str):
-    # The code for task 6 goes here. Once implemented, remove the line below that says "pass"
-    pass
+    with get_session() as db:
+        user = db.exec(select(User).where(User.username==username)).first()
+        if not user:
+            print(f'{username} not found. Failed to update email')
+            return
+        user.email = new_email
+        db.add(user)
+        db.commit()
+        print(f"Updated {user.username}'s email to {user.email}")
 
-@cli.command()
+#creating a new user
+@cli.command(help="Create a new user")
 def create_user(username: str, email:str, password: str):
-    # The code for task 7 goes here. Once implemented, remove the line below that says "pass"
-    pass
+    with get_session() as db:
+        newuser = User(username=username, email=email, password=password)
+        try: #contains the block that can throw an error
+            db.add(newuser)
+            db.commit()
+        except IntegrityError as e: #states what error to look out for
+            db.rollback() #the database can undo any erroneous operations and execute further queries.
+            print("Username or email already taken.")
+        else: #runs the code if the error do not occur.
+            print(newuser)
+#jacklyn and ronny was created
+#jacklyn, jacklyn@yahoo.com, jacky
+#ronny, ronny123@gmail.com, ronron45
 
-@cli.command()
+#deleting a user
+@cli.command(help="Delete a user by their username")
 def delete_user(username: str):
-    # The code for task 8 goes here. Once implemented, remove the line below that says "pass"
-    pass
+    with get_session() as db:
+        user = db.exec(select(User).where(User.username==username)).first()
+        if not user:
+            print(f'{username} not found. Failed to delete user')
+            return
+        db.delete(user)
+        db.commit()
+        print(f'{username} deleted.')
+#jacklyn was deleted
+
+#exercises
+#ex1
+@cli.command(help="Get a user by their username or email")
+def get_user(username: str, email:str):
+    with get_session() as db:
+        user = db.exec(select(User).where(or_(User.username.ilike(f"%{username}%"),(User.email.ilike(f"%{email}%"))))).first()
+        if not user:
+            print("User not found")
+        else:
+            print(user)
+
+#ex2
+@cli.command(help="Get a list of users with pagination")
+def get_user(limit: int=10, offset: int=0):
+    with get_session() as db:
+        user = db.exec(select(User).offset(offset).limit(limit)).all()
+        if not user:
+            print("Users not found")
+        else:
+            print(user)
 
 
 if __name__ == "__main__":
--- app/models.py
+++ app/models.py
@@ -4,8 +4,27 @@ from pwdlib import PasswordHash
 
 password_hash = PasswordHash.recommended()
 
+#if we want the class to be a table, specify table=True
+#id, username, email, password are fields
+#unique=True means the value cannot be repeated in another record
+#Optional[int] is a type hint, so the database will provide the ID nd SQLModel understand that it starts as None but becomes a non-null integer.
 class User(SQLModel, table=True):
     id: Optional[int] =  Field(default=None, primary_key=True)
     username:str = Field(index=True, unique=True)
     email:str = Field(index=True, unique=True)
     password:str
+
+#the constructor of the USER class, used to make class instance
+def __init__(self, username, email, password):
+    self.username = username
+    self.email = email
+    self.set_password(password)
+
+#method defined to hash user passwords
+def set_password(self, password):
+    self.password = password_hash.hash(password)
+
+#returns a string rep of an instance of the model to the console
+def __str__(self) -> str:
+    return f"(User id={self.id}, userame={self.username}, email={self.email})"
+
